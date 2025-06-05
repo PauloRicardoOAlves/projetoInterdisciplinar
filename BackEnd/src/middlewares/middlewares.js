14b9225329dccd737b0f22ae6e73ad94367cc1af
Paulo Ricardo Alves <pauloricardooalves@gmail.com>
2025-06-05 08:39:45 -0300

    adição do login

--- BackEnd/src/middlewares/middlewares.js
+++ BackEnd/src/middlewares/middlewares.js
@@ -1,5 +1,4 @@
 function blankValidation(req, res, next) {
-
     for (i in req.body) {
         if (!req.body[i]) {
             return res.status(400).json({ 'Erro': `o campo ${i} não pode estar em branco!` })
@@ -9,6 +8,27 @@ function blankValidation(req, res, next) {
     next()
 }
 
+async function login(req, res, next) {
+
+    const user = await Book.findOne({
+            where: {
+                id: id
+            }
+        })
+
+        if (!bookById) {
+            return res.status(404).json({ 'Erro': 'Nenhum usuário encontrado!' })
+        }
+
+    if (user) {
+        const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '1h' })
+
+        return res.json({ token })
+    }
+    return res.status(401).send('Credenciais inválidas.');
+}
+
 module.exports = {
-    blankValidation
+    blankValidation,
+    login
 }
